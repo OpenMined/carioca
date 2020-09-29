72d4d4b81c4c9522d1cc76e339d43c771863d1aa
Patrick Cason <me@patrickcason.com>
2020-09-29 17:43:56 +0100

    Changing a package and slightly modifying hot reload

--- packages/utils/src/hot-reload/server.js
+++ packages/utils/src/hot-reload/server.js
@@ -1,20 +1,53 @@
-const { join } = require('path');
+const path = require('path');
 const webpack = require('webpack');
 const clearModule = require('clear-module');
-const NodemonPlugin = require('nodemon-webpack-plugin');
 
 const { error } = require('../log');
 
+// A simpler helper function that allows us to define a new HTTP server
+// And keep track of any associated socket connections
+const initHttpServer = (bundlePath) => {
+  // Have a place to store the new server
+  let server;
+
+  try {
+    // Try to require the server from the bundle path
+    server = require(bundlePath).default;
+  } catch (e) {
+    // If we can't, log an error
+    error('Error creating server', e);
+    return null;
+  }
+
+  // Keep track of all the sockets in a Map
+  const sockets = new Map();
+  let nextSocketId = 0;
+
+  // When there's a socket connection
+  server.on('connection', (socket) => {
+    // Get the ID
+    const socketId = nextSocketId++;
+
+    // Add it to the list
+    sockets.set(socketId, socket);
+
+    // When that socket connection is closed, remove it from the list
+    socket.on('close', () => sockets.delete(socketId));
+  });
+
+  // Return both the required server and the Map of sockets
+  return { server, sockets };
+};
+
 module.exports = (serverConfig) => {
+  // Store the HTTP server somewhere
+  let httpServerObject;
+
   // Define the path of the server bundle
-  const bundlePath = join(
+  const bundlePath = path.join(
     serverConfig.output.path,
     serverConfig.output.filename
   );
-
-  // Add Nodemon to the serverConfig
-  if (!serverConfig.plugins) serverConfig.plugins = [new NodemonPlugin()];
-  else serverConfig.plugins.push(new NodemonPlugin());
 
   // Create a new Webpack compiler for the server
   const compiler = webpack(serverConfig);
@@ -33,7 +66,20 @@ module.exports = (serverConfig) => {
       return;
     }
 
-    // Each time we have new files, we should destroy the cached bundle
+    // Each time we have new files, we should destroy the cached version of the bundle
     clearModule(bundlePath);
+
+    // If we already have an HTTP server, kill it!
+    if (httpServerObject) {
+      httpServerObject.server.close(() => {
+        // ... and then destroy all open socket connections
+        for (const socket of httpServerObject.sockets.values()) {
+          socket.destroy();
+        }
+      });
+    }
+
+    // Initialize a new HTTP server
+    httpServerObject = initHttpServer(bundlePath);
   });
 };
