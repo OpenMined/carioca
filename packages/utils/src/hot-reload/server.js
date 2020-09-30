0e390c053285f6322e80dab8720ea3785a89bbcd
Patrick Cason <me@patrickcason.com>
2020-09-30 11:39:14 +0100

    Some degree of progress

--- packages/utils/src/hot-reload/server.js
+++ packages/utils/src/hot-reload/server.js
@@ -1,48 +1,11 @@
 const path = require('path');
+const fs = require('fs');
 const webpack = require('webpack');
-const clearModule = require('clear-module');
+const nodemon = require('nodemon');
 
 const { error } = require('../log');
 
-// A simpler helper function that allows us to define a new HTTP server
-// And keep track of any associated socket connections
-const initHttpServer = (bundlePath) => {
-  // Have a place to store the new server
-  let server;
-
-  try {
-    // Try to require the server from the bundle path
-    server = require(bundlePath).default;
-  } catch (e) {
-    // If we can't, log an error
-    error('Error creating server', e);
-    return null;
-  }
-
-  // Keep track of all the sockets in a Map
-  const sockets = new Map();
-  let nextSocketId = 0;
-
-  // When there's a socket connection
-  server.on('connection', (socket) => {
-    // Get the ID
-    const socketId = nextSocketId++;
-
-    // Add it to the list
-    sockets.set(socketId, socket);
-
-    // When that socket connection is closed, remove it from the list
-    socket.on('close', () => sockets.delete(socketId));
-  });
-
-  // Return both the required server and the Map of sockets
-  return { server, sockets };
-};
-
 module.exports = (serverConfig) => {
-  // Store the HTTP server somewhere
-  let httpServerObject;
-
   // Define the path of the server bundle
   const bundlePath = path.join(
     serverConfig.output.path,
@@ -65,21 +28,9 @@ module.exports = (serverConfig) => {
       error('Error bundling server', err);
       return;
     }
-
-    // Each time we have new files, we should destroy the cached version of the bundle
-    clearModule(bundlePath);
-
-    // If we already have an HTTP server, kill it!
-    if (httpServerObject) {
-      httpServerObject.server.close(() => {
-        // ... and then destroy all open socket connections
-        for (const socket of httpServerObject.sockets.values()) {
-          socket.destroy();
-        }
-      });
-    }
-
-    // Initialize a new HTTP server
-    httpServerObject = initHttpServer(bundlePath);
   });
+
+  if (fs.existsSync(bundlePath)) {
+    nodemon({ script: bundlePath, watch: [bundlePath] });
+  }
 };
